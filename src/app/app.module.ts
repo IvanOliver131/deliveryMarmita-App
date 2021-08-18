9da018279ed8a6822d48d0537ad6aa0d9f115109
ygor-salles <ygorsallesaniceto@gmail.com>
2021-08-17 23:23:43 -0300

    Ajustando vars de ambiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TopBarModule } from './top-bar/top-bar.module';
 import { FormsModule } from '@angular/forms';
-import { BrMaskerModule } from 'br-mask';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,8 +19,7 @@ import { BrMaskerModule } from 'br-mask';
     AppRoutingModule,
     HttpClientModule,
     TopBarModule,
-    FormsModule,
-    BrMaskerModule
+    FormsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://back-delivery.herokuapp.com/api',
+  productImagesServerUrl: 'https://back-delivery.herokuapp.com/api/'
 };
